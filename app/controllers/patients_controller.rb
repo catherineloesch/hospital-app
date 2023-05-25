6ec0ab8702ef4bdedb1d90488e817ea1f921514b
Catherine Loesch <catherine.loesch@protonmail.com>
2023-05-25 23:44:28 +0100

    add NEW route for patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,4 +6,8 @@ class PatientsController < ApplicationController
     def show
         @patient = Patient.find(params[:id])
     end
+
+    def new
+        @patient = Patient.new()
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "patients", to: "patients#index"
+  get "patients/new", to: "patients#new", as: :new_patient
   get "patients/:id", to: "patients#show", as: :patient 
-
+ 
 
   # Defines the root path route ("/")
   root "patients#index"
