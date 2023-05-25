269da8c97d4589b4842d5212892173d7b1100fb6
Catherine Loesch <catherine.loesch@protonmail.com>
2023-05-25 23:29:32 +0100

    add SHOW route for patient

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "patients", to: "patients#index"
+  get "patients/:id", to: "patients#show", as: :patient 
+
+
   # Defines the root path route ("/")
   root "patients#index"
 end
